53a0ac92ddba69ae6fc74da361fdbbbb465d69dd
rainishaagrawal <rainishaagrawal15@gmail.com>
2026-07-25 08:10:04 +0530

    Time: 1 ms (100%), Space: 43.8 MB (98.29%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,24 +1,30 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-        ArrayList<Integer> radiant = new ArrayList<>();
-        ArrayList<Integer> dire = new ArrayList<>();
-        int n = senate.length();
-        for (int i = 0; i < n; i++) {
-            if (senate.charAt(i) == 'R') {
-                radiant.add(i);
-            } else {
-                dire.add(i);
+        return simulateRec(senate.toCharArray(), 0, 0) ? "Radiant" : "Dire";
+    }
+    private static boolean simulateRec(char[] s, int rBan, int dBan) {
+        int r = 0, d = 0;
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] == 'R') {
+                if (dBan > 0) {
+                    dBan--;
+                    s[i] = 0;
+                } else {
+                    r++;
+                    rBan++;
+                }
+            } else if (s[i] == 'D') {
+                if (rBan > 0) {
+                    rBan--;
+                    s[i] = 0;
+                } else {
+                    d++;
+                    dBan++;
+                }
             }
         }
-        while (!radiant.isEmpty() && !dire.isEmpty()) {
-            int r = radiant.remove(0);
-            int d = dire.remove(0);
-            if (r < d) {
-                radiant.add(r + n);
-            } else {
-                dire.add(d + n);
-            }
-        }
-        return radiant.isEmpty() ? "Dire" : "Radiant";
+        if (r == 0) return false;
+        if (d == 0) return true;
+        return simulateRec(s, rBan, dBan);
     }
 }
