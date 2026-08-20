46784d12ccec7346956030db10604b8149bbf785
rainishaagrawal <rainishaagrawal15@gmail.com>
2026-08-20 08:29:37 +0530

    Time: 3 ms (58.11%), Space: 48.7 MB (48.19%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
@@ -1,27 +1,17 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
-        int n = nums.length;
-        int[] ans = new int[n];
-        ArrayList<Integer> even = new ArrayList<>();
-        ArrayList<Integer> odd = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (nums[i] % 2 == 0) {
-                even.add(nums[i]);
-            } else {
-                odd.add(nums[i]);
+        int i = 0, j = 1, n = nums.length;
+        while (i < n && j < n) {
+            if(nums[i] % 2 == 0) i+=2;
+            else if (nums[j] % 2 == 1) j+=2;
+            else {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i += 2;
+                j += 2;
             }
         }
-        int e = 0;
-        int o = 0;
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                ans[i] = even.get(e);
-                e++;
-            } else {
-                ans[i] = odd.get(o);
-                o++;
-            }
-        }
-        return ans;
+        return nums;
     }
 }
